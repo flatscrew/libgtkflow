842bed96eb565fa758668a35944032029bfe425e
grindhold <grindhold@gmx.net>
2021-04-07 13:36:06 +0200

    examples: update object.py

--- examples/objects.py
+++ examples/objects.py
@@ -25,41 +25,49 @@ class Point(object):
 
 class PointConstructorNode(Node):
     def __init__(self):
-        self.x_sink = GFlow.SimpleSink.new(float(0))
-        self.y_sink = GFlow.SimpleSink.new(float(0))
+        self.x_sink = GFlow.SimpleSink.with_type(float)
+        self.y_sink = GFlow.SimpleSink.with_type(float)
         self.x_sink.set_name("X")
         self.y_sink.set_name("Y")
+        self.x = 0.0
+        self.y = 0.0
         self.add_sink(self.x_sink)
         self.add_sink(self.y_sink)
 
-        self.result = GFlow.SimpleSource.new(Point())
+        self.result = GFlow.SimpleSource.with_type(object)
         self.result.set_name("result")
         self.add_source(self.result)
         self.result.set_value(None)
 
-        self.x_sink.connect("changed", self.do_calculations)
-        self.y_sink.connect("changed", self.do_calculations)
+        self.x_sink.connect("changed", self.do_calculations, "x")
+        self.y_sink.connect("changed", self.do_calculations, "y")
 
         self.set_name("Point Constructor")
 
-    def do_calculations(self, dock, val=None):
-        val_x = self.x_sink.get_value(0)
-        val_y = self.y_sink.get_value(0)
-        if val_x is None or val_y is None:
+    def do_calculations(self, dock, val=None, flow_id=None, affiliation=None):
+        if affiliation == "x":
+            self.x = val
+        elif affiliation == "y":
+            self.y = val
+        else:
+            return
+
+        if self.x is None or self.y is None:
             self.result.set_value(None)
             return
 
-        p = Point(x=val_x, y=val_y)
+        p = Point(x=self.x, y=self.y)
         self.result.set_value(p)
 
 class PointSplitterNode(Node):
     def __init__(self):
-        self.p_sink = GFlow.SimpleSink.new(Point())
+        self.p_sink = GFlow.SimpleSink.new(object)
         self.p_sink.set_name("point")
+        self.p = None
         self.add_sink(self.p_sink)
 
-        self.result_x = GFlow.SimpleSource.new(float(0))
-        self.result_y = GFlow.SimpleSource.new(float(0))
+        self.result_x = GFlow.SimpleSource.with_type(float)
+        self.result_y = GFlow.SimpleSource.with_type(float)
         self.result_x.set_name("X")
         self.result_y.set_name("Y")
         self.add_source(self.result_x)
@@ -71,20 +79,21 @@ class PointSplitterNode(Node):
 
         self.set_name("Point Splitter")
 
-    def do_calculations(self, dock, val=None):
-        val_p = self.p_sink.get_value(0)
-        if val_p is None:
+    def do_calculations(self, dock, val=None, flow_id=None):
+        self.p = val
+        if val is None:
             self.result_x.set_value(None)
             self.result_y.set_value(None)
             return
 
-        self.result_x.set_value(val_p.x)
-        self.result_y.set_value(val_p.y)
+        self.result_x.set_value(self.p.x)
+        self.result_y.set_value(self.p.y)
 
 
 class NumberNode(Node):
-    def __init__(self, number=0):
-        self.number = GFlow.SimpleSource.new(float(number))
+    def __init__(self, number=0.0):
+        self.number = GFlow.SimpleSource.with_type(float)
+        self.number.set_value(number)
         self.number.set_name("output")
         self.add_source(self.number)
 
@@ -102,7 +111,7 @@ class NumberNode(Node):
 
 class PrintNode(Node):
     def __init__(self):
-        self.number = GFlow.SimpleSink.new(float(0))
+        self.number = GFlow.SimpleSink.with_type(float)
         self.number.set_name("input")
         self.number.connect("changed", self.do_printing)
         self.add_sink(self.number)
@@ -111,10 +120,9 @@ class PrintNode(Node):
 
         self.set_name("Output")
 
-    def do_printing(self, dock):
-        n = self.number.get_value(0)
-        if n is not None:
-            self.childlabel.set_text(str(n))
+    def do_printing(self, dock, val=None, flow_id=None):
+        if val is not None:
+            self.childlabel.set_text(str(val))
         else:
             self.childlabel.set_text("")
 
